6f8178a8d355d2d52703de171b295a968fcb605e
drexx891 <andreksuek@gmail.com>
2026-06-23 16:16:41 +0700

    chore: setup vercel deployment scripts

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Cloning Flutter..."
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:`pwd`/flutter/bin"
+echo "Building Flutter Web..."
+flutter build web --release --web-renderer canvaskit
+echo "Copying output to public..."
+mkdir -p public
+cp -r build/web/* public/

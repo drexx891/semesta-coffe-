1c702fda5bb7b82c3125bf947b5bb99ec9443beb
drexx891 <andreksuek@gmail.com>
2026-06-23 16:22:39 +0700

    fix: remove deprecated web-renderer flag

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Cloning Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
 echo "Building Flutter Web..."
-flutter build web --release --web-renderer canvaskit
+flutter build web --release
 echo "Copying output to public..."
 mkdir -p public
 cp -r build/web/* public/

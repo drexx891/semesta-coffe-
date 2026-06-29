4a2f2cf7b94fa7b9c08cdff723fe97dc2e685312
drexx891 <andreksuek@gmail.com>
2026-06-29 17:35:48 +0700

    feat: migrate web deployment to github pages via actions

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Cloning Flutter..."
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
-export PATH="$PATH:`pwd`/flutter/bin"
-echo "Building Flutter Web..."
-flutter build web --release
-echo "Copying output to public..."
-mkdir -p public
-cp -r build/web/* public/

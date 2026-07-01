37efd5cd792ae6373fc714f769d6d2bf9020c6c3
GitHub Action <action@github.com>
2026-07-01 18:24:49 +0000

    [skip ci] chore: update public build for vercel

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "30251052" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1661845601" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
